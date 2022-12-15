3f578a0066b4b51ab83e598d3ecc9b4f9861bd43
Ram Mittala <mittalas@amazon.com>
2022-12-15 11:44:59 -0800

    Initial commit

---
+++ src/main/java/com/example/demospringbatch101/DemoSpringbatch101Application.java
@@ -0,0 +1,13 @@
+package com.example.demospringbatch101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbatch101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbatch101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringbatch101/DemoSpringbatch101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbatch101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbatch101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
